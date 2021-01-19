5db8f6c8cb38ce93f3b825ff33528ead1984b495
krishnakumarcn <krishnacnskp@gmail.com>
2021-01-19 22:45:25 +0530

    Added faded content

--- lib/faded.dart
+++ lib/faded.dart
@@ -1,7 +1,3 @@
 library faded;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:faded/src/faded.dart';
---
+++ lib/src/faded.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Faded extends StatefulWidget {
+  final String dueDate;
+  final int daysDeadline;
+  final Widget child;
+
+  const Faded(
+      {@required this.dueDate,
+      @required this.daysDeadline,
+      @required this.child});
+
+  @override
+  State<StatefulWidget> createState() => _FadedState();
+}
+
+class _FadedState extends State<Faded> {
+  @override
+  Widget build(BuildContext context) {
+    double opacity = getOpacity(widget.dueDate, widget.daysDeadline);
+    return Opacity(
+      opacity: opacity,
+      child: widget.child,
+    );
+  }
+
+  double getOpacity(String dueDate, int deadline) {
+    final _dueDate = DateTime.parse(dueDate);
+    double opacity = 1.0;
+    final now = DateTime.now();
+    if (now.isAfter(_dueDate)) {
+      int differenceInDays = DateTime.now().difference(_dueDate).inDays;
+      if (differenceInDays > deadline) {
+        opacity = 0.0;
+      } else {
+        double finishValue = (differenceInDays / deadline);
+        opacity = 1.0 - finishValue;
+      }
+    }
+    return opacity;
+  }
+}
