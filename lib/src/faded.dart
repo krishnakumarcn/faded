62d534bebdc257d07a19c356244c6d06ddf5c4ea
Jeremiah Oluwaseun Erinola <seunjeremiah@gmail.com>
2021-11-24 22:58:29 +0100

    Null Safety Added
    I upgraded the project SDK version and made the package null safety enabled.
    
    I wanted to use it for a project, but when i saw that it is not Null Safety, i needed to do the fix myself.

--- lib/src/faded.dart
+++ lib/src/faded.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Faded extends StatefulWidget {
+  /// Creates a widget that can be faded in and out.
+  /// The [child] argument must not be null.
+  /// The [opacity] argument must be a value between 0.0 and 1.0, inclusive.
   final String dueDate;
+  /// The dueDate is the date you want the widget to fade in and out.
   final int daysDeadline;
+  /// The daysDeadline is the number of days you want the widget to fade in and out.
+  /// It must also equate with the due date you are setting
   final Widget child;
+  /// The widget to display as the content of this widget.
 
   const Faded(
-      {@required this.dueDate,
-      @required this.daysDeadline,
-      @required this.child});
+      {required this.dueDate,
+      required this.daysDeadline,
+      required this.child});
 
   @override
   State<StatefulWidget> createState() => _FadedState();
@@ -17,6 +24,8 @@ class Faded extends StatefulWidget {
 class _FadedState extends State<Faded> {
   @override
   Widget build(BuildContext context) {
+    /// This function is a container that will fade in and out based on the due date and days deadline.
+    /// It will also display the child widget.
     double opacity = getOpacity(widget.dueDate, widget.daysDeadline);
     return Opacity(
       opacity: opacity,
