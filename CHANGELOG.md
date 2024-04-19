bed533a4e6dcad034e265137d24a20a90488edd2
krishnakumarcn <krishnacnskp@gmail.com>
2024-04-20 00:54:20 +0200

    dependency upgrade and DateTime

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,15 @@
+## [2.0.0] - Dependency upgrade. Breaking change for DueDate
+
+- Dart version upgraded to >=3.0.0
+- `dueDate` changed to a `DateTime` from `String` (it was a noobie library on inception🙂, now evolved into yet another
+  noobie library with `DateTime` 🤓 )
+
 ## [1.1.1] - Minor formatting changes
+
 ## [1.1.0] - Support null safety.
 
 ## [1.0.2] - Minor changes to documentation.
+
 ## [1.0.0] - Initial release.
-Get. set. Go..
+
 
--- test/faded_test.dart
+++ test/faded_test.dart
@@ -1,8 +1,27 @@
+import 'package:faded/faded.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('op check', () {
-    // double op = getOpacity('2021-01-18', 40);
-    // expect(op, 0.0);
+  ShadowFader shadowFader = ShadowFader();
+  final now = DateTime(2024, 04, 20);
+  final dueDateDeadlineVsResultMap = {
+    (DateTime(2024, 04, 18), 2): 0.0,
+    (DateTime(2024, 04, 17), 2): 0.0,
+    (DateTime(2024, 04, 19), 2): 0.5,
+    (DateTime(2024, 04, 20), 2): 1.0,
+    (DateTime(2024, 04, 21), 2): 1.0,
+    (DateTime(2212, 04, 21), 2): 1.0,
+    (DateTime(2000, 04, 21), 2): 0.0,
+  };
+
+  dueDateDeadlineVsResultMap.forEach((testCase, expected) {
+    final dueDate = testCase.$1;
+    final deadline = testCase.$2;
+    test(
+      'now: $now, due: $dueDate, deadline: $deadline ==> $expected',
+      () {
+        expect(shadowFader.hauntingOpacity(now, dueDate, deadline), expected);
+      },
+    );
   });
 }
